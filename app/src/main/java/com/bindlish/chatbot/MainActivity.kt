65e2955ca92d687575e541ae8edb66f8f795af88
Aman Bindlish <aman.bindlish@fliplearn.com>
2019-09-17 15:14:41 +0530

    initial commit

---
+++ app/src/main/java/com/bindlish/chatbot/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.bindlish.chatbot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Created by Aman Bindlish on 17,September,2019
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
